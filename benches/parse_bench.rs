0f76e00ca489618ad6dfe77893ff6f2fa70dcf74
Peter Krull <peterkrullpeter@gmail.com>
2023-01-31 16:34:58 +0100

    Change benchmarks and tests to only support original parser (see next commit)

--- benches/parse_bench.rs
+++ benches/parse_bench.rs
@@ -1,28 +1,14 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, Criterion};
 use hex_literal::hex;
 
-fn bench_parser_original(c: &mut Criterion) {
+pub fn bench_parser(c: &mut Criterion) {
 
     let mut parser = sbus::SBusPacketParser::new();
 
     let bytes =
         hex!("00 0F E0 03 1F 58 C0 07 16 B0 80 05 2C 60 01 0B F8 C0 07 00 00 00 00 00 03 00");
     
-    c.bench_function("parser_original", |b| b.iter(||{
-        parser.push_bytes(&bytes);
-        let msg = parser.try_parse_original().unwrap();
-        black_box(msg);
-    }));
-}
-
-fn bench_parser_bitvec(c: &mut Criterion) {
-
-    let mut parser = sbus::SBusPacketParser::new();
-
-    let bytes =
-        hex!("00 0F E0 03 1F 58 C0 07 16 B0 80 05 2C 60 01 0B F8 C0 07 00 00 00 00 00 03 00");
-    
-    c.bench_function("parser_bitvec", |b| b.iter(||{
+    c.bench_function("parser", |b| b.iter(||{
         
         parser.push_bytes(&bytes);
         let msg = parser.try_parse().unwrap();
@@ -30,10 +16,3 @@ fn bench_parser_bitvec(c: &mut Criterion) {
         black_box(msg);
     }));
 }
-
-
-criterion_group!(benches, 
-    bench_parser_original,
-    bench_parser_bitvec
-);
-criterion_main!(benches);
--- tests/parse_tester.rs
+++ tests/parse_tester.rs
@@ -3,30 +3,7 @@ mod tests {
     use hex_literal::hex;
 
     #[test]
-    fn test_sbus_parse_original() {
-
-        let mut parser = sbus::SBusPacketParser::new();
-
-        let bytes =
-            hex!("00 0F E0 03 1F 58 C0 07 16 B0 80 05 2C 60 01 0B F8 C0 07 00 00 00 00 00 03 00");
-        
-        parser.push_bytes(&bytes);
-        let parsed = parser.try_parse_original().unwrap();
-
-        assert_eq!(
-            sbus::SBusPacket {
-                channels: [992, 992, 352, 992, 352, 352, 352, 352, 352, 352, 992, 992, 0, 0, 0, 0],
-                d1: true,
-                d2: true,
-                failsafe: false,
-                frame_lost: false
-            },
-            parsed
-        );
-    }
-
-    #[test]
-    fn test_sbus_parse_bitvec() {
+    fn test_sbus_parse() {
 
         let mut parser = sbus::SBusPacketParser::new();
 
@@ -46,23 +23,5 @@ mod tests {
             },
             parsed
         );
-    }
-
-
-    #[test]
-    fn test_sbus_parse_same() {
-
-        let mut parser = sbus::SBusPacketParser::new();
-
-        let bytes =
-            hex!("00 0F E0 03 1F 58 C0 07 16 B0 80 05 2C 60 01 0B F8 C0 07 00 00 00 00 00 03 00");
-        
-        parser.push_bytes(&bytes);
-        let parsed_1 = parser.try_parse().unwrap();
-
-        parser.push_bytes(&bytes);
-        let parsed_2 = parser.try_parse_original().unwrap();
-
-        assert_eq!( parsed_1, parsed_2 );
     }
 }
